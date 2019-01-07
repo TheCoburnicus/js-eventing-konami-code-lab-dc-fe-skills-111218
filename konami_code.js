43553952908b1e2ce48fad0f4cf6ad2037bb3eb7
Nathan Coburn <nathanlcoburn@gmail.com>
2019-01-07 23:52:26 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
  let index = 0;
   
-  document.body.addEventListener('keydown", function(e)'){
+  document.body.addEventListener('keydown", function(e)'); {
     cont key = e.detail || e.which;
     
     console.log("key = " + key);
